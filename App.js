b0146ba2272f8b3853c6b2e5ec78ee4a3cd335eb
Rohit Saini <rohitsaini0444@gmail.com>
2025-12-23 15:49:20 +0530

    element inside components

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const element = <h1>This is React Element</h1>;
 const Title = () => <h1>This is Title Component</h1>;
 const Heading = () => {
   return (
     <div id="heading" className="head" tabIndex="5">
         <Title />
+        {element}
     </div>
   );
 }
