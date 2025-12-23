868a8392a37263f23a8fe30f04a41f6fd3dcf800
Rohit Saini <rohitsaini0444@gmail.com>
2025-12-23 15:21:47 +0530

    composit component

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement('div', { id: 'parent', key: 'parent' }, [
-    React.createElement('div', { id: 'child1', key: 'child1' },
-        [React.createElement('h1', { key: 'h1-1' }, "I'm an h1 tag"),
-        React.createElement('h2', { key: 'h2-1' }, "I'm an h2 tag")]),
-    React.createElement('div', { id: 'child2', key: 'child2' }, [
-        React.createElement('h1', { key: 'h1-2' }, "I'm an h1 tag"),
-        React.createElement('h2', { key: 'h2-2' }, "I'm an h2 tag")])
-]
-)
-// const heading = React.createElement("h1", { id: "heading" }, "Hello from react");
-const jsxHeading = <h1 id="heading" className="heading">Hello from JSX</h1>;
-
+const Title = () => <h1>This is Title Component</h1>;
+const Heading = () => {
+  return (
+    <div id="heading" className="head" tabIndex="5">
+        <Title />
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Heading />);
