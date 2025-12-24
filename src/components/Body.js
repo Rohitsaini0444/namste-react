0a617874da27f877deb81f59cb72a7a24867e43e
Rohit Saini <rohitsaini0444@gmail.com>
2025-12-24 19:19:02 +0530

    Used useState hook to add button for filtering top rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,27 @@
 import { restaurantList } from "../../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const BodyComponent = () => {
+    const [restaurants, setRestaurants] = useState(restaurantList);
     return (
         <div className="restaurant-container">
-            <div className="search-container">
-                <input type="text" className="search-box" />
-                <button className="search-btn">Search</button>
+            <div className="filter-area">
+                <div className="search-container">
+                    <input type="text" className="search-box" />
+                    <button className="search-btn">Search</button>
+                </div>
+                <div className="filter-container">
+                    <button className="filter-btn" onClick={()=>{
+                        console.log("Clicked", restaurantList);
+                        const filteredRestaurants = restaurantList.filter((res)=> res.card.card.info.avgRating>4.5);
+                        setRestaurants(filteredRestaurants);
+                    }}>Top Rated Restaurants</button>
+                </div>
             </div>
             <div className="restaurant-container">
-                {restaurantList.map((restaurant) => {
-                    if (restaurant?.card?.card?.info) {
-                        return <RestaurantCard key={restaurant.card.card.info?.id} resData={restaurant.card.card} />
-                    }
+                {restaurants.map((restaurant) => {
+                        return <RestaurantCard key={restaurant?.card?.card?.info?.id} resData={restaurant?.card?.card} />
                 })}
             </div>
         </div>
