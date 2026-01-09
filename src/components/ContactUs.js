b83191ec88351e7d987ac7a4a6d8990c987b9c7b
Rohit Saini <rohitsaini0444@gmail.com>
2026-01-09 15:22:18 +0530

    Created basic contactUs page with form

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,13 @@ const ContactComponent = () => {
     return (
         <div>
             <h1>Contact Us</h1>
-            <p>This is the contact page of our application.</p>
+            <input type="text" placeholder="Your Name" className="border p-2 m-2"/>
+            <br/>
+            <input type="text" placeholder="Your Email" className="border p-2 m-2"/> 
+            <br/>
+            <textarea placeholder="Your Message" className="border p-2 m-2"></textarea>
+            <br/>
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded m-2">Submit</button>
         </div>
     );
 }
